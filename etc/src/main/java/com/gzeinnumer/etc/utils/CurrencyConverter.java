a745acdf8ccdbe9d2be85689d99ecc542fcd15cf
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-07-13 13:42:32 +0700

    provide emptystring

--- etc/src/main/java/com/gzeinnumer/etc/utils/CurrencyConverter.java
+++ etc/src/main/java/com/gzeinnumer/etc/utils/CurrencyConverter.java
@@ -104,7 +104,7 @@ public class CurrencyConverter implements TextWatcher {
                 } else {
                     formattedString = formatInteger(cleanString);
                 }
-       re         editText.removeTextChangedListener(this); // Remove listener
+                editText.removeTextChangedListener(this); // Remove listener
                 editText.setText(formattedString);
                 handleSelection();
                 if (stringCallBack != null)
